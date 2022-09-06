b3e6dc30aa2583e94f17a83f64dc566521553aeb
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-09-06 17:27:54 +0300

    done

--- probe.py
+++ probe.py
@@ -1,4 +1,7 @@
-def truncate(word, index):
-    return word[:index - 1]
+def get_age_difference(year1, year2):
+    diff = year2 - year1
+    stroka = 'The age difference is'
+    strola1 = stroka + ' ' + str(diff)
+    return strola1
 
-print(truncate('hexlet', 2))
+print(get_age_difference(2001, 2018))
