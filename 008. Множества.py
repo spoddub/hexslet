0b08c3fe789f6083413e710cedb2bf78a5168e6d
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-09-18 15:53:53 +0300

    ррешенное задание по словарям

---
+++ 008. Множества.py
@@ -0,0 +1,22 @@
+# Вам предстоит реализовать функцию all_unique(),
+# которая должна принимать итератор (в т.ч. и те, которые не перезапускаемые!) и возвращать True,
+# если элементы в итераторе не повторяются (если элементов нет, то ничего не повторяется!). Пример работы функции:
+#
+# all_unique([])
+# # True
+# all_unique([1, 2, 3])
+# # True
+# all_unique(iter([1, 2, 3]))
+# # True
+# all_unique([1, 2, 1])
+# # False
+
+def all_unique(iterator):
+    counter = 0
+    items = set()
+    for item in iterator:
+        items.add(item)
+        counter += 1
+        if len(items) != counter:
+            return False
+    return True
