a3d41425f31dab2e7e08a4b622847b50e7dbddfb
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-09-28 00:20:14 +0300

    бинарный поиск

--- probe.py
+++ probe.py
@@ -1,15 +1,10 @@
-def reverse_string(string):
-    index = len(string) - 1
-    reversed_string = ''
+def greet(name, *args):
+    for n in (name,) + args:
+        print(f'Hello, {n}!')
 
-    while index >= 0:
-        current_char = string[index]
-        reversed_string = reversed_string + current_char
-        # То же самое через интерполяцию
-        # reversed_string = f'{reversed_string}{current_char}'
-        index = index - 1
+names = ['Tom', 'Ann']
 
-    return reversed_string
-
-print(reverse_string('Game Of Thrones'))
-
+greet(
+   'Bob', *['Mary', 'Clair'], 'Sam',
+   *('Henry', 'John')
+)
